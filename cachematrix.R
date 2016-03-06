4524d9a26de9036adc2a3a3e70b8e154d4e3ed35
ameyapangarkar <ameyapangarkar@users.noreply.github.com>
2016-03-06 11:24:39 +0530

    Updated cacheMatrix.R to contain actual functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix:
+## This function creates a list of functions containing:
+## setMatrix = Function to set new Matrix
+## getMatrix = Function to get current Matrix
+## setinvMatrix = Function to set inv of Matrix
+## getinvMatrix = Function to get current inv of Matrix
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(M = matrix()) {
 
-makeCacheMatrix <- function(x = matrix()) {
-
+    # Initialize inv Matrix to NULL
+    iM <- NULL
+    
+    ## Function to set new Matrix, make inverse NULL
+    setMatrix <- function (x) {
+        M <<- x
+        iM <<- NULL
+    }
+    
+    ## Function to return current Matrix
+    getMatrix <- function () {
+        M
+    }
+    
+    ## Function to set inv Matrix
+    setinvMatrix <- function (ix) {
+        iM <<- ix
+    }
+    
+    ## Function to return current inv Matrix
+    getinvMatrix <- function () {
+        iM
+    }
+    
+    list(setMatrix = setMatrix, getMatrix = getMatrix, 
+         setinvMatrix = setinvMatrix, getinvMatrix = getinvMatrix)
+    
 }
 
 
-## Write a short comment describing this function
+## cacheSolve:
+## This function returns inverse of matrix from cache if available
+## If not available the function sets inv of matrix and returns it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(fl, ...) {
+    
+    ## Get current value of inv Matrix
+    iX <- fl$getinvMatrix()
+    
+    ## If current value is not null, then retun the value
+    if(!is.null(iX)) {
+        message("Found inverse matrix in cache")
+        return(iX)
+    }
+    
+    ## If current value is null, then calculate new value of inv Matrix
+    X <- fl$getMatrix()
+    iX <- solve(X, ...)
+    fl$setinvMatrix(iX)
+    iX
 }
